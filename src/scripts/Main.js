cf1173ed547a0bcf3bcd78a7eac8322a496f5964
Jeremy Bourg <jer.bourg2@gmail.com>
2026-02-02 18:02:13 -0500

    initial commit

---
+++ src/scripts/Main.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper/bundle';
+import ComponentFactory from './ComponentFactory.js';
+import Icons from './utils/Icons.js';
+
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    document.documentElement.classList.add('has-js');
+
+    Icons.load();
+    new ComponentFactory();
+
+    var swiper = new Swiper('.mySwiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+}
+
+new Main();
