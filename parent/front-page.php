ccc227b1b460455b1f3d5756350a85cb6f471a83
Jérémy Bourg <jer.bourg2@gmail.com>
2026-02-03 00:21:10 -0500

    move function definitions to functions.php

--- parent/front-page.php
+++ parent/front-page.php
@@ -1,38 +1,39 @@
 <?php get_header(); ?>
 
-<?php $placeholder = get_bloginfo("template_url") . "/strawberry.jpg" ?>
+<section class="hero">
+    <div class="wrapper">
+        <div class="hero__media">
+			<img src=<?php echo $placeholder ?>"" alt="Skieur étoile" />
+        </div>
+        <div class="hero__content">
+            <h1>
+                La
+                <span>crème</span>
+                <br />
+                de la crème
+            </h1>
+            <p>
+            text
+            </p>
+            <a href="#" class="button">Joignez notre équipe</a>
+        </div>
+    </div>
+</section>
 
-<?php if(have_rows("blocks")): ?>
-<?php while(have_rows("blocks")): the_row(); ?>
-<?php if(get_row_layout() == "gallery"): ?>	
-	<div class="section section--no-pb">
-		<div class="wrapper">
-			<div class="section__header">
-				<h2>Image</h2>
-			</div>
-		</div>
-		<div class="swiper mySwiper">
-			<div class="swiper-wrapper">
-				<?php while(have_rows("gallery")): the_row(); ?>
-					<?php $image = get_sub_field("image"); ?>
-					<div class="swiper-slide">
-						<?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
-					</div>
+<?php
+/* render blocks */
 
-				<?php endwhile; ?>
-			</div>
-			<div class="swiper-pagination"></div>
-		</div>
-	</div>
-<?php elseif(get_row_layout() == "cta"): ?>
-	<div class="section section--no-pb">
-		<div class="wrapper">
-			<?php the_sub_field('content'); ?>
-			<a href="<?php the_sub_field('lien_vers'); ?>">En savoir plus</a>
-		</div>
-	</div>
-<?php endif; ?>
-<?php endwhile; ?>
-<?php endif; ?>
+if(have_rows("blocks")):
+	while(have_rows("blocks")): the_row();
+
+		if(get_row_layout() == "gallery"):
+			echo render_gallery();
+		elseif(get_row_layout() == "cta"):
+			echo render_cta();
+		endif;
+
+	endwhile;
+endif;
+?>
 
 <?php get_footer(); ?>
--- parent/index.php
+++ parent/index.php
@@ -1,49 +1,5 @@
 <?php get_header(); ?>
 
-<?php $placeholder = get_bloginfo("template_url") . "/strawberry.jpg" ?>
-
-<?php
-/* function definitions for blocks */
-
-function render_gallery() {
-	ob_start();
- ?>
-	<div class="section section--no-pb">
-		<div class="wrapper">
-			<div class="section__header">
-				<h2>Image</h2>
-			</div>
-		</div>
-		<div class="swiper mySwiper">
-			<div class="swiper-wrapper">
-				<?php while(have_rows("gallery")): the_row(); ?>
-					<?php $image = get_sub_field("image"); ?>
-					<div class="swiper-slide">
-						<?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
-					</div>
-				<?php endwhile; ?>
-			</div>
-			<div class="swiper-pagination"></div>
-		</div>
-	</div>
-<?php
-	return ob_get_clean();
-}
-
-function render_cta() {
-	ob_start();
- ?>
-	<div class="section section--no-pb">
-		<div class="wrapper">
-			<?php the_sub_field('content'); ?>
-			<a href="<?php the_sub_field('lien_vers'); ?>">En savoir plus</a>
-		</div>
-	</div>
-<?php
-	return ob_get_clean();
-}
- ?>
-
 <?php
 /* render blocks */
 
