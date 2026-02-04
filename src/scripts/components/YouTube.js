b532fe8eab6346b4cebcf1c63944a66efcaf32ff
Jeremy Bourg <jer.bourg2@gmail.com>
2026-02-04 08:27:56 -0500

    youtube test

--- src/scripts/components/YouTube.js
+++ src/scripts/components/YouTube.js
@@ -4,11 +4,10 @@ export default class YouTube {
 		this.videoContainer = this.element.querySelector('.js-video');
 		this.poster = this.element.querySelector('.js-poster');
 		this.videoId = this.element.dataset.videoId;
-		this.autoplay = this.poster ? 1 : 0;
 		this.playerReady = false;
-
 		this.options = {
-			controls: 1
+			rel: 0,
+			autoplay: this.poster ? 1 : 0,
 		};
 
 		YouTube.instances.push(this);
@@ -16,13 +15,20 @@ export default class YouTube {
 		if (this.videoId) {
 			YouTube.loadScript();
 		} else {
-			console.error('Vous devez spécifier un id');
+			console.error("Impossible d'initialiser vidéo sans ID youtube.");
+		}
+	}
+
+	setOptions() {
+		if ('noControls' in this.element.dataset) {
+			this.options.controls = 0;
 		}
 	}
 
 	static loadScript() {
-		if (!YouTube.scriptIsLoading) {
-			YouTube.scriptIsLoading = true;
+		if (!YouTube.isScriptLoading) {
+			YouTube.isScriptLoading = true;
+
 			const script = document.createElement('script');
 			script.src = 'https://www.youtube.com/iframe_api';
 			document.body.appendChild(script);
@@ -30,40 +36,31 @@ export default class YouTube {
 	}
 
 	init() {
-		this.setControls();
-
+		this.setOptions();
 		this.initPlayer = this.initPlayer.bind(this);
 
 		if (this.poster) {
 			this.element.addEventListener('click', this.initPlayer);
 		} else {
-			this.initPlayer();
+			this.initPlayer().bind(this);
 		}
 	}
-
-
-	setControls(){
-		if('noControls' in this.element.dataset){
-			console.log('no-controls');
-			this.options.controls = '0';
-		}
-	}
-
-
 
 	initPlayer(event) {
 		if (event) {
 			this.element.removeEventListener('click', this.initPlayer);
 		}
 
+		console.log(this.videoId);
 		this.player = new YT.Player(this.videoContainer, {
 			height: '100%',
 			width: '100%',
 			videoId: this.videoId,
-			playerVars: { rel: 0, autoplay: this.autoplay, controls: this.options.controls },
+			playerVars: this.options,
 			events: {
 				onReady: () => {
 					this.playerReady = true;
+
 					const observer = new IntersectionObserver(this.watch.bind(this), {
 						rootMargin: '0px 0px 0px 0px',
 					});
@@ -87,20 +84,20 @@ export default class YouTube {
 		}
 	}
 
-	static initAll() {
-		document.documentElement.classList.add('is-video-ready');
-
-		for (let i = 0; i < YouTube.instances.length; i++) {
-			const instance = YouTube.instances[i];
-			instance.init();
-		}
-	}
 	static pauseAll(currentInstance) {
 		for (let i = 0; i < YouTube.instances.length; i++) {
 			const instance = YouTube.instances[i];
 			if (instance.playerReady && instance !== currentInstance) {
 				instance.player.pauseVideo();
 			}
+		}
+	}
+	static initAll() {
+		document.documentElement.classList.add('is-video-ready');
+
+		for (let i = 0; i < YouTube.instances.length; i++) {
+			const instance = YouTube.instances[i];
+			instance.init();
 		}
 	}
 }
