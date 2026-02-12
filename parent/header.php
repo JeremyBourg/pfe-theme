10b720f4f194a07375ff231da1a6e4356444fe45
Jérémy Bourg <jer.bourg2@gmail.com>
2026-02-11 22:24:26 -0500

    fix <title> element

--- parent/header.php
+++ parent/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<title><?php bloginfo('name'); ?>></title>
+	<title><?php bloginfo('name'); ?></title>
 
 	<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/dist/styles/main.css" />
 
